a7c90ec1483d350226148e017073d0ac0f98ab5b
Daniel Robbins <drobbins@funtoo.org>
2011-09-22 00:36:35 +0000

    preserve bindist through use expansion

--- pym/portage/package/ebuild/config.py
+++ pym/portage/package/ebuild/config.py
@@ -1690,11 +1690,16 @@ class config(object):
 				use.add("targetroot")
 			else:
 				use.discard("targetroot")
+		got_bindist = "bindist" in use
 
 		# Allow _* flags from USE_EXPAND wildcards to pass through here.
 		use.difference_update([x for x in use \
 			if (x not in explicit_iuse and \
 			not iuse_implicit_match(x)) and x[-2:] != '_*'])
+
+		# bindist is special and preserved:
+		if got_bindist:
+			use.add("bindist")
 
 		# Use the calculated USE flags to regenerate the USE_EXPAND flags so
 		# that they are consistent. For optimal performance, use slice
