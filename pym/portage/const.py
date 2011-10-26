8ce3337b1ac0c8a3f407c6a4dc93354a764ab95d
Daniel Robbins <drobbins@funtoo.org>
2011-10-26 15:58:13 +0000

    add mini-manifest back to SUPPORTED_FEATURES so Portage doesn't complain about mini-manifest when enabled. This is just a convenience tweak; the mini-manifest feature doesn't do anything anymore in this branch.

--- pym/portage/const.py
+++ pym/portage/const.py
@@ -103,7 +103,7 @@ SUPPORTED_FEATURES       = frozenset([
                            "strict", "stricter", "suidctl", "test", "test-fail-continue",
                            "unknown-features-filter", "unknown-features-warn",
                            "unmerge-logs", "unmerge-orphans", "userfetch", "userpriv",
-                           "usersandbox", "usersync", "webrsync-gpg"])
+                           "usersandbox", "usersync", "webrsync-gpg", "mini-manifest"])
 
 EAPI                     = 4
 
